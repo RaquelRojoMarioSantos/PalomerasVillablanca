362a3043ecdd7cf333a00d97bdefa9c8efebac53
Miembro2 <mario.santos.animacion3d@gmail.com>
2021-03-19 19:18:52 +0100

    Version 3

--- Assets/Scripts/ScrBoton.cs
+++ Assets/Scripts/ScrBoton.cs
@@ -7,12 +7,14 @@ public class ScrBoton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Este es un cambio");
+
+
     }
 }
